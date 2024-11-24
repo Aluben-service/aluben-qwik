2da72c7dc17adedc72e9bbdd734b93837399ab55
I don’t codddde <120886940+asc2563@users.noreply.github.com>
2024-11-24 09:15:16 -0800

    fi

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,8 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
     webServer: {
         command: 'npm run dev',
-        port: 5173
+        port: 5173,
+        reuseExistingServer: true
     },
 
     testDir: 'e2e',
