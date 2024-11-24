fb38658f604d277b18b674e90356589c14292888
Coding4Hours <susybaka@mail.com>
2024-11-24 15:25:44 -0800

    jfjlkdsfasdfasfasdf

--- init.ts
+++ init.ts
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.PUBLIC_FIREBASE_API_KEY ,
-  authDomain: "aluben-2794e.firebaseapp.com",
-  projectId: "aluben-2794e",
-  storageBucket: "aluben-2794e.firebasestorage.app",
-  messagingSenderId: "851804241341",
-  appId: "1:851804241341:web:9c74e26bc5c338ab0b26d4",
-  measurementId: "G-VQHMN7DCGN"
+  apiKey: import.meta.env.FIREBASE_API_KEY,
+  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.FIREBASE_APP_ID,
+  measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
